0d89919080642e346412c3fd4e923c76fb1b7141
Sylvie-Wxr <sylvie_wu125@outlook.com>
2025-05-06 23:34:29 -0700

    show basic task details

--- task-manager/app/task/[id].tsx
+++ task-manager/app/task/[id].tsx
@@ -1,9 +1,43 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, TextInput, Button } from "react-native-paper";
+import { useLocalSearchParams, useRouter, useNavigation } from "expo-router";
+import { useLayoutEffect, useState } from "react";
+import { useTaskContext } from "../../context/TaskContext";
 
 export default function TaskDetailScreen() {
+	const { id } = useLocalSearchParams(); // get dynamic id
+	const { tasks } = useTaskContext();
+  const router = useRouter();
+  const navigation = useNavigation();
+
+	const task = tasks.find((t) => t.id === id);
+
+	// Dynamically set the screen title
+  useLayoutEffect(() => {
+    if (task) {
+      navigation.setOptions({ title: task.title });
+    }
+  }, [task]);
+
+  if (!task) {
+    return <Text>Task not found</Text>;
+  }
+
 	return (
-		<View>
-			<Text> Task Details</Text>
+		<View style={styles.container}>
+			<Text variant="titleLarge">{task.title}</Text>
+			<Text>{task.description}</Text>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  input: {
+    marginBottom: 16,
+  },
+});
