8c03e8357b2884c88a563aac3e044b31c191459b
Sylvie-Wxr <sylvie_wu125@outlook.com>
2025-05-08 17:59:36 -0700

    beautify add task

--- task-manager/app/add.tsx
+++ task-manager/app/add.tsx
@@ -1,13 +1,14 @@
 import { useRouter } from "expo-router";
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, View } from "react-native";
+import { Button, Text, TextInput, RadioButton } from "react-native-paper";
 import { Task } from "../types/task";
 import { useTaskContext } from "../context/TaskContext";
 
 export default function AddTaskScreen() {
 	const [title, setTitle] = useState("");
 	const [description, setDescription] = useState("");
+	const [status, setStatus] = useState<"pending" | "completed">("pending");
 	const router = useRouter();
 	// Access addTask function from context
 	const { addTask } = useTaskContext();
@@ -15,63 +16,134 @@ export default function AddTaskScreen() {
 	// Handle save button
 	const handleSave = () => {
 		// Create a new task object
-    const newTask: Task = {
-      id: Date.now().toString(), // use Date to create id
-      title,
-      description,
-      status: "pending",
-    };
+		const newTask: Task = {
+			id: Date.now().toString(), // use Date to create id
+			title,
+			description,
+			status,
+		};
 
 		// Add the task using context method
 		addTask(newTask);
+		// console.log("Saving task with status:", status);
 		router.back(); // Navigate back to the task list screen
 	};
 
 	return (
-	<View style={styles.container}>
-		<Text variant="titleLarge" style={styles.header}>New Task</Text>
-
-		<TextInput
-			label="Title"
-			value={title}
-			onChangeText={setTitle}
-			mode="outlined"
-			style={styles.input}
-		/>
-
-		<TextInput
-			label="Description"
-			value={description}
-			onChangeText={setDescription}
-			mode="outlined"
-			multiline
-			numberOfLines={3}
-			style={styles.input}
-		/>
-
-		{/* Save Button */}
-		<Button
-			mode="contained"
-			onPress={handleSave}
-			disabled={title.trim() === ""} // Disable if title is empty
+		<KeyboardAvoidingView
+			behavior={Platform.OS === "ios" ? "padding" : "height"}
+			style={{ flex: 1 }}
 		>
-			Save
-		</Button>
-	</View>
+			<ScrollView contentContainerStyle={styles.container}>
+				<View style={styles.headerContainer}>
+					<Text style={styles.headerText}>New Task</Text>
+				</View>
+
+
+				<View style={styles.inputGroup}>
+					<Text style={styles.label}>Title</Text>
+					<TextInput
+						mode="outlined"
+						placeholder="e.g., Grocery Shopping"
+						value={title}
+						onChangeText={setTitle}
+						style={styles.input}
+					/>
+				</View>
+
+
+				<View style={styles.inputGroup}>
+					<Text style={styles.label}>Description</Text>
+					<TextInput
+						mode="outlined"
+						placeholder="Details about the task..."
+						value={description}
+						onChangeText={setDescription}
+						multiline
+						numberOfLines={6}
+						style={styles.discrptiontextArea}
+					/>
+				</View>
+
+				<View style={styles.inputGroup}>
+					<Text style={styles.label}>Status</Text>
+					<RadioButton.Group onValueChange={value => setStatus(value as "pending" | "completed")} value={status}>
+						<View style={styles.radioRow}>
+							<RadioButton value="pending" />
+							<Text>Pending</Text>
+						</View>
+						<View style={styles.radioRow}>
+							<RadioButton value="completed" />
+							<Text>Completed</Text>
+						</View>
+					</RadioButton.Group>
+				</View>
+
+				{/* Save Button */}
+				<Button
+					mode="contained"
+					onPress={handleSave}
+					buttonColor="#8c7cde"
+					disabled={title.trim() === ""}
+					style={styles.submitButton}
+				>
+					Save
+				</Button>
+			</ScrollView>
+		</KeyboardAvoidingView>
 	);
 }
-  
-  const styles = StyleSheet.create({
-    container: {
-      padding: 16,
-      flex: 1,
-      backgroundColor: "#fff",
-    },
-    header: {
-      marginBottom: 16,
-    },
-    input: {
-      marginBottom: 16,
-    },
-  })
-;
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 20,
+		flex: 1,
+		backgroundColor: "#fff",
+	},
+	headerContainer: {
+		backgroundColor: "#f6f2f9",
+		paddingVertical: 20,
+		paddingHorizontal: 20,
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		position: "relative",
+		marginBottom: 40,
+		borderRadius: 16,
+	},
+
+	headerText: {
+		fontSize: 18,
+		fontWeight: "bold",
+		textAlign: "center",
+		flex: 1,
+	},
+
+	inputGroup: {
+		marginBottom: 25,
+	},
+	label: {
+		marginBottom: 10,
+		fontSize: 14,
+		fontWeight: "600",
+	},
+	input: {
+		backgroundColor: "#fff",
+	},
+	discrptiontextArea: {
+		backgroundColor: "#fff",
+		height: 160,
+		textAlignVertical: "top",
+	},
+	radioRow: {
+		flexDirection: "row",
+		alignItems: "center",
+		marginTop: 6,
+	},
+	submitButton: {
+		marginTop: 24,
+		paddingVertical: 6,
+		color: "#bb91de",
+	},
+})
+	;
